1d1c292530c99a81a503335bb16f21fe59494739
kobedead <kobe.frateur@gmail.com>
2025-03-27 03:00:37 +0100

    updated controller

--- src/main/java/ds/namingnote/Controller/NodeController.java
+++ src/main/java/ds/namingnote/Controller/NodeController.java
@@ -2,16 +2,19 @@ package ds.namingnote.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -25,44 +28,46 @@ public class NodeController {
 
 
     @GetMapping("/file/{filename}")
-    public ResponseEntity getFile(@PathVariable String filename) throws FileNotFoundException {
+    public ResponseEntity<Resource> returnFile(@PathVariable String filename) throws FileNotFoundException {
 
-        Path path = Paths.get("Files/"+filename);
+        Path path = Paths.get("Files/" + filename);
 
-        if (!Files.exists(path)){
-            return new ResponseEntity("FIle Not Found",HttpStatus.NOT_FOUND);
-
+        // Check if file exists
+        if (Files.notExists(path)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "File Not Found");
         }
 
-        // Creating new file instance
-        File file= new File(path.toString());
-        // Creating a new InputStreamResource object
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        try {
+            File file = path.toFile();
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
 
-        // Creating a new instance of HttpHeaders Object
-        HttpHeaders headers = new HttpHeaders();
+            // Try to determine the content type dynamically
+            String contentType = Files.probeContentType(path);
+            if (contentType == null) {
+                contentType = "application/octet-stream";  // Default fallback
+            }
 
-        // Setting up values for contentType and headerValue
-        String contentType = "application/octet-stream";
-        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+                    .body(resource);
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
-                .body(resource);
-
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error reading file", e);
+        }
     }
 
+
     @PostMapping("/file/")
-    public ResponseEntity uploadFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException {
-        
+    public ResponseEntity<String> downloadFile(@RequestParam("file") MultipartFile file) throws FileNotFoundException {
 
         try {
-            // Process the file (e.g., save it to disk, store in database, etc.)
+            // Save the file on disc
             String fileName = file.getOriginalFilename();
             File destFile = new File("Files", fileName);
             System.out.println("File saved to: " + destFile.getAbsolutePath());
             file.transferTo(destFile.toPath());
+
             return ResponseEntity.ok("File uploaded successfully: " + fileName);
 
         } catch (Exception e) {
@@ -70,9 +75,6 @@ public class NodeController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to upload the file");
         }
-
-
-
     }
 
 

773e01c8294439701dfe5e57a7b1a895892401ab
s0193227 <eli.wauters@student.uantwerpen.be>
2025-04-15 19:03:38 +0200

    enabled scheduling

--- src/main/java/ds/namingnote/NamingNoteApplication.java
+++ src/main/java/ds/namingnote/NamingNoteApplication.java
@@ -9,9 +9,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableScheduling
 public class NamingNoteApplication {
 
     public static void main(String[] args) {

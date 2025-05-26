e67de45d61765c54f0f98b2aa9b102f0ace91830
s0193227 <eli.wauters@student.uantwerpen.be>
2025-05-27 00:28:40 +0200

    Revert "Shutdown"
    This reverts commit c3f179da0f09928ae38ba83921d1199ae05b082b.

--- src/main/java/ds/namingnote/Multicast/MulticastSender.java
+++ src/main/java/ds/namingnote/Multicast/MulticastSender.java
@@ -36,7 +36,7 @@ public class MulticastSender implements Runnable {
             }
 
             try {
-                Thread.sleep(5000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt(); // Restore interrupted status
                 System.out.println("Multicast thread interrupted.");
